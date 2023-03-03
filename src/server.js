b28d213fbcbc2217598e0a00803f29eded411596
shikharagrawal2002 <partywithshikhar@gmail.com>
2023-03-03 13:44:35 +0530

    Update

--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const serverless = require('serverless-http');
-const { MONGO_URI } = require('../config');
+const express = require("express");
+const mongoose = require("mongoose");
+const serverless = require("serverless-http");
+const { MONGO_URI } = require("../config");
 
 // const { model } = require('mongoose');
 const router = express.Router();
 
 //posts Model
-const Posts = require('../models/Posts');
+const Posts = require("../models/Posts");
 
 // @routes GET api/posts
 // @desc GET all posts
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const posts = await Posts.find();
-    if (!posts) throw Error('No Items');
+    if (!posts) throw Error("No Items");
     res.status(200).json(posts);
   } catch (err) {
     res.status(400).json({ msg: err });
@@ -26,19 +26,16 @@ router.get('/', async (req, res) => {
 
 const app = express();
 
-// BodyParser Middleware
-// app.use(express.json());
-
 // Connect to MongoDB
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('MongoDb Connected!'))
+  .then(() => console.log("MongoDb Connected!"))
   .catch((err) => console.log(err));
 
 // Use routes
-app.use('/.netlify/functions/server', router);
+app.use("/.netlify/functions/server", router);
 
 module.exports.handler = serverless(app);
